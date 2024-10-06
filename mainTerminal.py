4bc17eddc949c456f61adbfdfa08514f4f353e0e
khoa0428 <89560662+khoa0428@users.noreply.github.com>
2024-10-06 21:11:57 +0700

    Create mainTerminal.py
    Write your normal codeHS Karl(the dog) code in the section #Main Code Here

---
+++ mainTerminal.py
@@ -0,0 +1,24 @@
+from khoaPackage import *
+from KarlSimulationv2 import *
+
+level = 0
+
+clearTerminal()
+loadLevel(level)
+reload()
+
+#Main Code Here
+
+
+
+
+
+
+
+
+
+
+
+#----------------
+levelCheck(level)
+
